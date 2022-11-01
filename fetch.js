44f2ebaf38cf03a3650b6a706a27fbaf36533e4d
bruno <bruno_am_22@hotmail.com>
2022-11-01 10:10:21 -0300

    Actualizado

--- fetch.js
+++ fetch.js
@@ -1,7 +1,22 @@
 function mostrarResultados(dato) {
-   const precio = document.querySelector(".result-item-price");
-   console.log(dato[0].price);
-   //precio.innerHTML = dato[0].price;
+   const results  =document.querySelector(".result")
+   const template = document.querySelector("#result-template")
+   const resultCountSearch= document.querySelector(".cantidad-result")
+   resultCountSearch.textContent = dato.length;
+   for(let result of dato){
+      const precio = template.content.querySelector(".result-item-price");
+      let title =template.content.querySelector(".result-item-title")
+      let condition = template.content.querySelector(".result-item-condition")
+      let cantidadVentas = template.content.querySelector(".result-item-ventas")
+      let clone = document.importNode(template.content,true)
+      console.log(results);
+      results.splice(1).appendChild(clone)
+      condition.textContent = result.condition;
+      title.textContent = result.title;
+      precio.textContent = "$"+result.price;
+      cantidadVentas.textContent = result["sold_quantity"]
+   }
+   console.log(dato[0]);
 }
 
 function main() {
